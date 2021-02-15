4bc71b490870496b68e18680c0089c36ca030ed9
YangZZou <77215016+YangZZou@users.noreply.github.com>
2021-02-16 09:30:18 +1000

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World again from GitHub - update by git push through SSH to GitHub PW reset");
+		System.out.println("Hello World again from GitHub - update by git push through SSH to GitHub WebHook");
 	}
 }
